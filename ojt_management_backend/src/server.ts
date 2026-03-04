5b32afb5f551ea275efd82b069a2a92b54701140
SrushtiRindhe09 <rindhesrushti2006@gmail.com>
2026-03-04 22:36:59 +0530

    created backend folder

---
+++ ojt_management_backend/src/server.ts
@@ -0,0 +1,21 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import connectDB from "./config/db";
+
+dotenv.config();
+
+const app = express();
+connectDB();
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("OJT Management Backend Running 🚀");
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
